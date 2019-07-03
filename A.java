4c9302f898cd5a3d33fb8c698b2d50910926ed67
sahand.safizadeh <sahandsafizadeh.s@gmail.com>
2019-07-03 15:27:35 +0430

    still binary logical expressions can't be of other types

--- A.java
+++ A.java
@@ -2,17 +2,7 @@ public class A {
 
     public static void main(String[] args) {
         int a = 5;
-        switch (a) {
-            case 10:
-                return;
-            case 2:
-                break;
-            case 5:
-                System.out.println("hell");
-                break;
-            case 1:
-                a = 10;
-        }
+        boolean b = 0.5 > a;
     }
 
 }
--- src/ast/expr/binary/logical/LogicalBinaryExpr.java
+++ src/ast/expr/binary/logical/LogicalBinaryExpr.java
@@ -6,6 +6,7 @@ import ast.expr.binary.BinaryExpression;
 import ast.type.CastingType;
 import ast.type.Type;
 import ast.type.TypeChecker;
+import ast.type.VariableType;
 import cg.Logger;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.Opcodes;
@@ -24,8 +25,11 @@ public class LogicalBinaryExpr extends BinaryExpression {
 
     @Override
     public Node compile() {
-        Type resultType = TypeChecker.binaryExprTypeCheck(expr1.getType(), expr2.getType());
-        if (resultType != CastingType.INT)
+        Expression e1 = (Expression) expr1.compile();
+        Expression e2 = (Expression) expr2.compile();
+
+        Type resultType = TypeChecker.binaryExprTypeCheck(e1.getType(), e2.getType());
+        if (resultType != VariableType.INT)
             Logger.error("unsupported logical operation");
 
         Label l1 = new Label();
@@ -34,7 +38,7 @@ public class LogicalBinaryExpr extends BinaryExpression {
         mVisit.visitInsn(Opcodes.ICONST_1);
         mVisit.visitJumpInsn(Opcodes.GOTO, l2);
         mVisit.visitLabel(l1);
-        mVisit.visitInsn(Opcodes.ICONST_1);
+        mVisit.visitInsn(Opcodes.ICONST_0);
         mVisit.visitLabel(l2);
         return new LogicalBinaryExpr(resultType);
     }
