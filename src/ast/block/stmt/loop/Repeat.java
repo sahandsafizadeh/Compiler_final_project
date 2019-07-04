077feffc5627629e8e03d84863fbcf82401b92ed
sahand.safizadeh <sahandsafizadeh.s@gmail.com>
2019-07-04 19:19:49 +0430

    repeat until semantics finished

--- src/ast/block/stmt/loop/Repeat.java
+++ src/ast/block/stmt/loop/Repeat.java
@@ -1,21 +1,31 @@
 package ast.block.stmt.loop;
 
 import ast.Node;
-import ast.block.Blocks;
+import ast.block.Block;
 import ast.block.stmt.Statement;
+import ast.expr.Expression;
 import cg.CodeGenerator;
 import cg.Logger;
 import org.objectweb.asm.Opcodes;
 
 public class Repeat extends Statement {
 
+    private Block block;
+    private Expression expr;
+
+    public Repeat(Block block, Expression expr) {
+        this.block = block;
+        this.expr = expr;
+    }
+
     @Override
     public Node compile() {
         Logger.log("repeat");
-        Blocks blocks = Blocks.getInstance();
-        CodeGenerator.mVisit.visitJumpInsn(Opcodes.IFEQ, blocks.getCurrent().getStart());
-        blocks.getCurrent().compile();
-        return null;
+        block.markStart();
+        block.compile();
+        expr.compile();
+        CodeGenerator.mVisit.visitJumpInsn(Opcodes.IFEQ, block.getStart());
+        return this;
     }
 
 }
